2e9ee8293653ab691e7c59a4f9c375c03c264f8d
levkotman <leoktmn@yandex.ru>
2021-04-13 00:58:04 +0200

    fixed not playing bug
    removed function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 drum-kit
+Play drums on Vanilla JS
--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 "use strict";
-for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
-    let drumBtn = document.querySelectorAll('.drum')[i];
-    drumBtn.addEventListener('click', musicDrum(drumBtn));
-}
+let drumBtns = document.querySelectorAll(".drum");
 
-function musicDrum(drum) {
-    switch (drum.innerText) {
-        case "w":
-            let drumSoundW = new Audio('sounds/tom-1.mp3');
-            drumSoundW.play();
-        case "a":
-            let drumSoundA = new Audio('sounds/snare.mp3');
-            drumSoundA.play();
-        case "s":
-            let drumSoundS = new Audio('sounds/tom-3.mp3');
-            drumSoundS.play();
-        case "d":
-            let drumSoundD = new Audio('sounds/kick-bass.mp3');
-            drumSoundD.play();
-        case "j":
-            let drumSoundJ = new Audio('sounds/tom-4.mp3');
-            drumSoundJ.play();
-        case "k":
-            let drumSoundK = new Audio('sounds/tom-2.mp3');
-            drumSoundK.play();
-        case "l":
-            let drumSoundL = new Audio('sounds/crash.mp3');
-            drumSoundL.play();
+drumBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    switch (btn.textContent) {
+      case "w":
+        let drumSoundW = new Audio("sounds/tom-1.mp3");
+        drumSoundW.play();
+        break;
+      case "a":
+        let drumSoundA = new Audio("sounds/snare.mp3");
+        drumSoundA.play();
+        break;
+      case "s":
+        let drumSoundS = new Audio("sounds/tom-3.mp3");
+        drumSoundS.play();
+        break;
+      case "d":
+        let drumSoundD = new Audio("sounds/kick-bass.mp3");
+        drumSoundD.play();
+        break;
+      case "j":
+        let drumSoundJ = new Audio("sounds/tom-4.mp3");
+        drumSoundJ.play();
+        break;
+      case "k":
+        let drumSoundK = new Audio("sounds/tom-2.mp3");
+        drumSoundK.play();
+        break;
+      case "l":
+        let drumSoundL = new Audio("sounds/crash.mp3");
+        drumSoundL.play();
+        break;
+      default:
+        console.log("error button");
     }
-}
+  });
+});
